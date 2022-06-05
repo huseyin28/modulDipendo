92d3c09c096d287c9838975d7e491ffc1afcc9c7
Hüseyin Yılmaz <huseyin2219@gmail.com>
2022-06-05 22:18:11 +0300

    sanırım etiket işlemi tamam

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const expressLayouts = require('express-ejs-layouts')
 const path = require('path');
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const fs = require('fs');
 
 app.use("/public",express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(expressLayouts)
 app.set('view engine', 'ejs')
@@ -20,6 +25,29 @@ app.get('/', (req, res) => {
 
 app.get('/detay', (req, res) => {
     res.render('detay', { layout : false})
+})
+
+app.post('/createEtiket', (req, res) => {
+    let data = `${req.body.sipno}\n`+
+    req.body.customer+"\n"+
+    req.body.size+"\n"+
+    req.body.tanim+"\n"+
+    req.body.metraj+"\n"+
+    req.body.agirlik+"\n"+
+    req.body.pid
+    console.log(req.body);
+    fs.writeFile(path.join(__dirname,"/etiket.txt"), data, err => {
+        if (err) return console.log(err);
+        console.log('işlem başarılı');
+    })
+    res.json({success : true})
+})
+
+app.get('/detay2', (req, res) => {
+    res.render('detay2', { 
+        title: 'CelsanApp | Detay2',
+        scripts : `<script src="/public/scripts/detay2.js"></script>`
+    })
 })
 
 app.listen(port)
--- public/scripts/detay.js
+++ public/scripts/detay.js
@@ -1,25 +1,24 @@
-$(document).ready(ready)
-let Authorization = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiJ9.eyJuYW1laWQiOiI0MyIsInVuaXF1ZV9uYW1lIjoiaHVzZXlpbnlpbG1hekBjZWxzYW5jZWxpay5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL2FjY2Vzc2NvbnRyb2xzZXJ2aWNlLzIwMTAvMDcvY2xhaW1zL2lkZW50aXR5cHJvdmlkZXIiOiJBU1AuTkVUIElkZW50aXR5IiwiQXNwTmV0LklkZW50aXR5LlNlY3VyaXR5U3RhbXAiOiJiZTZhZWFhMS1mOThmLTQ5MmMtODViNy00YzU0NzdiZWI5MDQiLCJTdG9jayI6IjEiLCJFZGl0VXNlcnMiOiIxIiwiVmlld0Nvc3QiOiIxIiwiRWRpdEN1c3RvbWVycyI6IjEiLCJWaWV3UmVwb3J0cyI6IjEiLCJQdXJjaGFzZSI6IjEiLCJFZGl0UHVyY2hhc2VzIjoiMSIsIkVkaXRQdXJjaGFzZUl0ZW1zIjoiMSIsIkVkaXRQdXJjaGFzZUl0ZW1zUHVyY2hhc2VDb3VudCI6IjEiLCJTYWxlIjoiMSIsIkVkaXRTYWxlcyI6IjEiLCJWaWV3U2FsZXMiOiIxIiwiRWRpdFNhbGVJdGVtcyI6IjEiLCJDYWxlbmRhciI6IjEiLCJFZGl0U3VwcGxpZXJzIjoiMSIsIkVkaXRQcm9kdWN0cyI6IjEiLCJUZW5hbnQiOiI4MTAyNmRhMC0xMjQ4LTRkNWMtYjc3OC1iN2YxNjg3YzBhOWMiLCJDdWx0dXJlIjoidHItVFIiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L0RpcGVuZG8uQVBJLyIsImF1ZCI6ImM5MmQ1ZTQ1OGI1NmUyNDMyOGVmMzE1NWEwZGU1YmE3IiwiZXhwIjoxNjUzNDU3NDM3LCJuYmYiOjE2NTA4NjU0Mzd9.nRj9A7cCKl2WEhlwyxE_VWrEAA1INzQMNYAQamk_sRS2r9XTd8BsOWi7ulOVf65zN28KJ4AY4NjXxhjzyx3Nog`;
+let Authorization = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiJ9.eyJuYW1laWQiOiI0MyIsInVuaXF1ZV9uYW1lIjoiaHVzZXlpbnlpbG1hekBjZWxzYW5jZWxpay5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL2FjY2Vzc2NvbnRyb2xzZXJ2aWNlLzIwMTAvMDcvY2xhaW1zL2lkZW50aXR5cHJvdmlkZXIiOiJBU1AuTkVUIElkZW50aXR5IiwiQXNwTmV0LklkZW50aXR5LlNlY3VyaXR5U3RhbXAiOiJiZTZhZWFhMS1mOThmLTQ5MmMtODViNy00YzU0NzdiZWI5MDQiLCJTdG9jayI6IjEiLCJFZGl0VXNlcnMiOiIxIiwiVmlld0Nvc3QiOiIxIiwiRWRpdEN1c3RvbWVycyI6IjEiLCJWaWV3UmVwb3J0cyI6IjEiLCJQdXJjaGFzZSI6IjEiLCJFZGl0UHVyY2hhc2VzIjoiMSIsIkVkaXRQdXJjaGFzZUl0ZW1zIjoiMSIsIkVkaXRQdXJjaGFzZUl0ZW1zUHVyY2hhc2VDb3VudCI6IjEiLCJTYWxlIjoiMSIsIkVkaXRTYWxlcyI6IjEiLCJWaWV3U2FsZXMiOiIxIiwiRWRpdFNhbGVJdGVtcyI6IjEiLCJDYWxlbmRhciI6IjEiLCJFZGl0U3VwcGxpZXJzIjoiMSIsIkVkaXRQcm9kdWN0cyI6IjEiLCJUZW5hbnQiOiI4MTAyNmRhMC0xMjQ4LTRkNWMtYjc3OC1iN2YxNjg3YzBhOWMiLCJDdWx0dXJlIjoidHItVFIiLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0L0RpcGVuZG8uQVBJLyIsImF1ZCI6ImM5MmQ1ZTQ1OGI1NmUyNDMyOGVmMzE1NWEwZGU1YmE3IiwiZXhwIjoxNjU1MjIxNzc0LCJuYmYiOjE2NTI2Mjk3NzR9.nzyl2Nj8U6UqAsTodXugavYlOCp_q1llhbJc4cJdw2qb6ujCdKVrxZNkXdRRuBzUdLr1ApD4OF6p5QljpkOe-Q`;
 let units = `{"896":"m","897":"m","898":"m","899":"m","900":"m","901":"m","902":"m","903":"m","904":"m","905":"m","906":"m","907":"m","908":"m","909":"m","910":"m","911":"m","912":"m","913":"m","914":"m","915":"adet","916":"adet","917":"adet","918":"adet","919":"adet","920":"adet","921":"adet","922":"m","923":"adet","924":"adet","925":"adet","926":"adet","927":"adet","928":"adet","929":"adet","930":"adet","931":"adet","932":"adet","933":"kg","934":"adet","935":"adet","936":"adet","937":"kg","943":"adet","944":"adet"}`;
+let Sale = null
 
-function ready() {
-    Login();
-}
+$(document).ready(Login)
 
 function Login() {
     units = JSON.parse(units)
     const urlParams = new URLSearchParams(location.search);
     $.ajax({
         url: "https://app.dipendo.com/api/sales/" + urlParams.get('id'),
-        headers: { "Authorization": localStorage.getItem("Authorization") }
+        headers: { "Authorization": Authorization }
     }).then(response => {
+        Sale = response
         FORM.createPrintForm(response);
-        PAGE.writeForm(response);
+        PAGE.writeForm(response, urlParams.get('id'));
     })
 }
 
 let PAGE = {
-    writeForm: function (response) {
+    writeForm: function (response, sid) {
         $('#HtmlForm #htmlCustomer').html(response.customer.title)
         $('#HtmlForm #htmlSendDate').html(`Sevk Tarihi : ${FORM.getTarih(response.deliveryTime)}`)
         $('#HtmlForm #htmlExplanation').html(response.explanation)
@@ -47,13 +46,38 @@ let PAGE = {
         }
     },
     getRow: function (item) {
+        let btnCreateEtiket = `<button type="button" onclick="PAGE.createEtiket(${item.saleItemId})" class="btn btn-link btn-sm p-0">OLUŞTUR</button>`
         return `<div class="row">
-        <div class="col-8">${item.purchaseItem.product.name}</div>
-        <div class="col-1">${units[item.purchaseItem.product.productGroupId] == "m" ? item.purchaseItemId : ""}</div>
-        <div class="col-1">${(item.purchaseItem.product.unitMass * item.saleCount).toFixed(2)} ${item.purchaseItem.product.unitOfMass}</div>
-        <div class="col-1">${item.saleCount} ${units[item.purchaseItem.product.productGroupId]}</div>
-        <div class="col-1"><button type="button" class="btn btn-link btn-sm p-0">OLUŞTUR</button></div>
-    </div>`;
+            <div class="col-8">${item.purchaseItem.product.name}</div>
+            <div class="col-1">${units[item.purchaseItem.product.productGroupId] == "m" ? item.purchaseItemId : ""}</div>
+            <div class="col-1">${(item.purchaseItem.product.unitMass * item.saleCount).toFixed(2)} ${item.purchaseItem.product.unitOfMass}</div>
+            <div class="col-1">${item.saleCount} ${units[item.purchaseItem.product.productGroupId]}</div>
+            <div class="col-1">${units[item.purchaseItem.product.productGroupId] == "m" ? btnCreateEtiket : ""}</div>
+        </div>`;
+    },
+    createEtiket : function(saleItemId){
+        Sale.saleItems.forEach(element => {
+            if(element.saleItemId == saleItemId){
+                let data = {
+                    "sipno" :  Sale.externalSaleCode,
+                    "customer" : Sale.customer.title,
+                    "size" : element.purchaseItem.product.productPropertyValues[0].propertyValue,
+                    "tanim" : element.purchaseItem.product.productPropertyValues[1].propertyValue +' '+element.purchaseItem.product.productPropertyValues[2].propertyValue +' '+ element.purchaseItem.product.productPropertyValues[4].propertyValue +' '+ element.purchaseItem.product.productPropertyValues[6].propertyValue,
+                    "metraj" : element.saleCount,
+                    "agirlik" : (element.purchaseItem.product.unitMass * element.saleCount).toFixed(0),
+                    "pid" : element.purchaseItemId
+                }
+
+                $.ajax({
+                    type : "POST",
+                    url : "/createEtiket",
+                    data : data
+                }).then(response => {
+
+                })
+                return;
+            }
+        });
     },
     getHead: function () {
         return `<div class="row">
