c10568b918507a3eb69f4ff55f3075b23d1d1038
Hüseyin Yılmaz <huseyin2219@gmail.com>
2023-11-25 16:24:57 +0300

    bug düzeltme

--- public/scripts/qrcodescan.js
+++ public/scripts/qrcodescan.js
@@ -1,11 +1,16 @@
 $('#sidebarToggle').trigger('click')
+let control = true;
 
 var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10 });
+
 html5QrcodeScanner.render(function (decodedText, decodedResult) {
-    let txt = decodedText.replaceAll('dipendo://', '')
-    txt = txt.split('?id=')
-    if (txt[0] == 'info')
-        window.open('/purchaseItem/detay/' + txt[1], '_blank', 'noopener, noreferrer');
-    else if (txt[0] == 'sale')
-        window.open('/detay?id=' + txt[1], '_blank', 'noopener, noreferrer');
+    if (control) {
+        let txt = decodedText.replaceAll('dipendo://', '')
+        txt = txt.split('?id=')
+        if (txt[0] == 'info')
+            window.open('/purchaseItem/detay/' + txt[1], '_blank', 'noopener, noreferrer');
+        else if (txt[0] == 'sale')
+            window.open('/detay?id=' + txt[1], '_blank', 'noopener, noreferrer');
+        control = false
+    }
 });
