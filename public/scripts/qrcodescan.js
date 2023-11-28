80d070e481d732e77de01f1a4efd7495f3b91237
Hüseyin Yılmaz <huseyin2219@gmail.com>
2023-11-28 19:27:00 +0300

    qr scanner back

--- public/scripts/qrcodescan.js
+++ public/scripts/qrcodescan.js
@@ -1,19 +1,21 @@
 $('#sidebarToggle').trigger('click')
+let control = true;
 var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10 });
 
 $(window).on("focus", function () {
     html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 10 });
+    control = true;
 })
 
 
 html5QrcodeScanner.render(function (decodedText, decodedResult) {
-    let txt = decodedText.replaceAll('dipendo://', '')
-    txt = txt.split('?id=')
-    if (txt[0] == 'info')
-        window.open('/purchaseItem/detay/' + txt[1], '_blank', 'noopener, noreferrer');
-    else if (txt[0] == 'sale')
-        window.open('/detay?id=' + txt[1], '_blank', 'noopener, noreferrer');
-
-    html5QrcodeScanner.clear();
-
+    if (control) {
+        let txt = decodedText.replaceAll('dipendo://', '')
+        txt = txt.split('?id=')
+        if (txt[0] == 'info')
+            window.open('/purchaseItem/detay/' + txt[1], '_blank', 'noopener, noreferrer');
+        else if (txt[0] == 'sale')
+            window.open('/detay?id=' + txt[1], '_blank', 'noopener, noreferrer');
+        control = false
+    }
 });
