3955fb43f898787ce23c0d4b8c4b5555a6edaf2f
Hüseyin Yılmaz <huseyin2219@gmail.com>
2024-01-04 23:43:17 +0300

    depo giriş ile qrcode print bağlantısı yapıldı

--- public/scripts/girisBekleyen.js
+++ public/scripts/girisBekleyen.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     getList();
+    $('#btnOnay').off('click').on('click', printQRCodes)
 });
 
 function getList() {
@@ -9,18 +10,35 @@ function getList() {
     }).then(writeList).fail(ajaxFail)
 }
 
+function printQRCodes() {
+    let ids = []
+    let checkList = $('#tblList input[type="checkbox"]:checked');
+    for (let i = 0; i < checkList.length; i++) {
+        ids.push($(checkList[i]).val())
+    }
+    window.open('/printQRCode?codes=' + ids.join(','), '_blank');
+}
+
 function writeList(list) {
     $('#tblList').html('')
     list.forEach((element, i) => {
         $('#tblList').append(`<tr>
             <th scope="row">${i + 1}</th>
             <td>${element.product.groupName}</td>
+            <td>
+                <div class="form-check mr-1">
+                    <input class=" form-check-input" type="checkbox" value="${element.purchaseItemId}" id="${element.purchaseItemId}">
+                    <label class="form-check-label" for="${element.purchaseItemId}">${element.purchaseItemId}</label>
+                </div>
+            </td>
             <td>${element.product.name}</td>
             <td>${element.purchaseCount} ${getUnit(element.product.groupUnit)}</td>
-            <td><button type="button" class="btn btn-primary btn-sm" onclick="inStock(${element.purchaseItemId})">Depoya Al</button></td>
+            <td class="p-1"><button type="button" class="btn btn-primary btn-sm" onclick="inStock(${element.purchaseItemId})">Depoya Al</button></td>
         </tr>`)
     });
 }
+
+
 
 function inStock(purchaseItemId) {
     try {
