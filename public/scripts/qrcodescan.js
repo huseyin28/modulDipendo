e2377346a25c1572f3e118bc54d94684d85f8dce
Hüseyin Yılmaz <huseyin2219@gmail.com>
2024-03-31 10:00:01 +0300

    qrcode scanner update

--- public/scripts/qrcodescan.js
+++ public/scripts/qrcodescan.js
@@ -1,21 +1,18 @@
 $('#sidebarToggle').trigger('click')
-let control = true;
-var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 1 });
+var html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 5 });
 
 $(window).on("focus", function () {
-    html5QrcodeScanner = new Html5QrcodeScanner("qr-reader", { fps: 1 });
-    control = true;
+    html5QrcodeScanner.resume();
 })
 
 
 html5QrcodeScanner.render(function (decodedText, decodedResult) {
-    if (control) {
-        let txt = decodedText.replaceAll('dipendo://', '')
-        txt = txt.split('?id=')
-        if (txt[0] == 'info')
-            window.open('/purchaseItem/detay/' + txt[1], '_blank', 'noopener, noreferrer');
-        else if (txt[0] == 'sale')
-            window.open('/detay?id=' + txt[1], '_blank', 'noopener, noreferrer');
-        control = false
-    }
+    html5QrcodeScanner.pause(true);
+
+    let txt = decodedText.replaceAll('dipendo://', '')
+    txt = txt.split('?id=')
+    if (txt[0] == 'info')
+        window.open('/purchaseItem/detay/' + txt[1], '_blank', 'noopener, noreferrer');
+    else if (txt[0] == 'sale')
+        window.open('/detay?id=' + txt[1], '_blank', 'noopener, noreferrer');
 });
