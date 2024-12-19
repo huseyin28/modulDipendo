ce6d2ef9604755e33eeb387533970adada057070
Hüseyin Yılmaz <huseyin2219@gmail.com>
2024-12-19 17:29:34 +0300

    user db

--- controllers/DBManager.js
+++ controllers/DBManager.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createPool({
     connectionLimit: 10,
     host: '213.142.150.14',
-    user: 'hyyaevff',
+    user: 'hyyaevff_huseyin',
     password: 'M*THb4KSk-G8',
     database: 'hyyaevff_celsan'
 })
