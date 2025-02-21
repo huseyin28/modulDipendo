1ebf34028f35afef43c15d4afbfd987490178ce4
Hüseyin Yılmaz <huseyin2219@gmail.com>
2025-02-21 15:51:15 +0300

    sale update olarak değiştirilecek

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -1,11 +1,15 @@
 let connection = require('./DBManager')
 let ResponseObj = require('./ResponseObj')
+const sharp = require("sharp");
+const path = require('path')
+const db = require('../controllers/DBManager');
+const fs = require('fs')
 
 module.exports.getById = (req, res) => {
     let response = new ResponseObj()
     try {
         connection.query('SELECT * FROM sale_images WHERE saleId = ?', [req.params.id], (error, results, fields) => {
-            if (error)  
+            if (error)
                 response.setError(error)
             else
                 response.setData(results)
@@ -15,4 +19,47 @@ module.exports.getById = (req, res) => {
         response.setError(error.message)
         res.json(response)
     }
+}
+
+module.exports.imgUpload = async (req, res) => {
+    let response = new ResponseObj()
+    try {
+        const { persons, saleId } = req.body;
+        let filename = '';
+        if (!req.files || !req.files.images) { 
+            insertDB(saleId, filename, persons);
+        } else  { 
+            filename = saleId + '.' + req.files.images.name.split('.').at(-1);
+            uploadSaleImg(req.files.images, filename, saleId, persons)
+        } 
+        response.setData('OK')
+        res.json(response)
+    } catch (error) {
+        response.setError(error)
+        console.log(error)
+        res.json(response)
+    }
+}
+
+function uploadSaleImg(file, filename, saleId, selected_names) {
+    let uploadTempPath = path.join(__dirname, '..', 'public', 'images', 'sales', 'temp', filename)
+    let uploadPath = path.join(__dirname, '..', 'public', 'images', 'sales', filename)
+    
+    file.mv(uploadTempPath, (err) => {
+        if (!err) {
+            insertDB(saleId, filename, selected_names)
+            sharp(uploadTempPath).resize(1000).jpeg({ quality: 70 }).toFile(uploadPath).then(() => fs.unlinkSync(uploadTempPath)).catch(err => console.error("Hata:", err));
+        } else
+            console.error(err)
+    })
+}
+
+function insertDB(saleId, filename, preparers) {
+    db.query("SELECT * FROM sale_images WHERE saleId = ?", [saleId], (err, results) => {
+        if (err) throw err;
+        if (results.length > 0)
+            console.log("Ürünler:", results.length);
+        else
+            db.query(`INSERT INTO sale_images (saleId, filename, preparers) VALUES (?, ?, ?)`, [Number(saleId), filename, preparers]);
+    });
 }
--- routes/UploadsRouter.js
+++ routes/UploadsRouter.js
@@ -1,57 +1,7 @@
 const express = require('express');
-const sharp = require("sharp");
 const router = express.Router();
-const path = require('path')
-const pool = require('../controllers/DBManager');
-const ResponseObj = require('../controllers/ResponseObj')
+const saleController = require('../controllers/saleController')
 
-router.post('/sale', async function (req, res) {
-    let response = new ResponseObj()
-    try {
-        const { persons, saleId } = req.body;
-        let uploadPath = '';
-        let filename = '';
-        if (!req.files || !req.files.images) { // img seçilmediyse 
-            insertDB(saleId, filename, uploadPath, persons);
-        } else if (!req.files.images.length) { // Eğer tek img seçildiyse obj olarak gelicek
-            filename = saleId + '.' + req.files.images.name.split('.').at(-1);
-            uploadPath = path.join(__dirname, '..', 'public', 'images', 'sales', filename)
-            uploadSaleImg(req.files.images, filename, uploadPath, saleId, persons)
-        } else { // birden fazla img seçildiyse dizi şeklinde gelecek
-            for (let i = 0; i < req.files.images.length; i++) {
-                filename = `${saleId}-${i}.` + req.files.images[i].name.split('.').at(-1);
-                uploadPath = path.join(__dirname, '..', 'public', 'images', 'sales', filename)
-                uploadSaleImg(req.files.images[i], filename, uploadPath, saleId, persons)
-            }
-        }
-        response.setData('OK')
-    } catch (error) {
-        response.setError(error)
-        console.log(error);
-    }finally{
-        res.json(response)
-    }
-})
-
-function uploadSaleImg(file, filename, uploadPath, saleId, selected_names) {
-    file.mv(uploadPath, (err) => {
-        if (!err){
-            insertDB(saleId, filename, uploadPath, selected_names)
-            sharp(uploadPath)
-            .resize(800)
-            .jpeg({ quality: 70 })
-            .toFile(path.join(__dirname, '..', 'public', 'images', 'sales', 'lite', filename))
-            .then(() => console.log("Görsel sıkıştırıldı!"))
-            .catch(err => console.error("Hata:", err));
-        }else
-            console.error(err)
-    })
-}
-
-function insertDB(saleId, file_name, file_path, selected_names) {
-    pool.query(
-        `INSERT INTO sale_images (saleId, file_name, file_path, selected_names) VALUES (?, ?, ?,?)`,
-        [Number(saleId), file_name, file_path, selected_names]);
-}
+router.post('/sale', saleController.imgUpload)
 
 module.exports = router;
