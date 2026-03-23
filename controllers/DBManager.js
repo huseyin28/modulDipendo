742c8b5acd6da4ea89cc360ca0e02641cdfb6adc
Hüseyin Yılmaz <huseyin2219@gmail.com>
2026-03-23 09:53:26 +0300

    localhost deneme

--- controllers/DBManager.js
+++ controllers/DBManager.js
@@ -1,10 +1,20 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
     connectionLimit: 10,
-    host: '91.217.119.88',
-    user: 'hyyaevff',
-    password: 'M*THb4KSk-G8',
+    host: 'localhost',
+    user: 'hyyaevff_huseyin',
+    password: 'Huseyin2219',
     database: 'hyyaevff_celsan'
-})
+});
+
+// Bağlantıyı test etmek için ekleyin
+connection.getConnection((err, conn) => {
+    if (err) {
+        console.error('Bağlantı hatası:', err);
+    } else {
+        console.log('Bağlantı başarılı');
+        conn.release();
+    }
+});
 
 module.exports = connection
